eee971cd9b891d3cb3779fb37359e8f8d42b06f3
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-10-20 01:24:15 +0530

    Mock Interview 2.2

---
+++ 35.sql
@@ -0,0 +1,13 @@
+SELECT extract(dow FROM created_a) AS weekday
+	,count(CASE 
+			WHEN event_name = 'post'
+				THEN 1
+			ELSE NULL
+			END) * 1.00 / count(CASE 
+			WHEN event_name = 'enter'
+				THEN 1
+			ELSE NULL
+			END) * 100 AS perc_success
+FROM post_events
+GROUP BY 1
+ORDER BY 1
