271906d2e01785e64cf8c51390d26bea782b774d
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2022-01-24 21:04:42 +0530

    Update 45.sql

--- 45.sql
+++ 45.sql
@@ -1,4 +1,4 @@
-Revenue Over Time [Twitter]
+Revenue Over Time [Amazon]
 Find the 3-month rolling average of total revenue from purchases given a table with users, their purchase amount, and date purchased. Do not include returns which are represented by negative purchase values. Output the year-month (YYYY-MM) and 3-month rolling average of revenue, sorted from earliest month to latest month.
 
 A 3-month rolling average is defined by calculating the average total revenue from all user purchases for the current month and previous two months. The first two months will not be a true 3-month rolling average since we are not given data from last year. Assume each month has at least one purchase.
